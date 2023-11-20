a860bc5500ce1d124e9c202457b32672829167a2
Zonin <business.kariukimartin@gmail.com>
2023-11-20 16:46:23 +0300

    creating super user with crud methods

--- hello_world/members/admin.py
+++ hello_world/members/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Member
 # Register your models here.
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('firstName', "lastName", "joined_date")
+
+
+admin.site.register(Member, MemberAdmin)
